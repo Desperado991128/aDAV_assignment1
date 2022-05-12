02463b39b635cb1324b23f353f059d9de609523f
WilliamADSR <Williamschaafsma@gmail.com>
2022-05-12 17:40:15 +0200

    Initial push

---
+++ assignment_1.R
@@ -0,0 +1,10 @@
+library(foreign)
+library(tidyverse)
+library(mice)
+library(readr)
+
+obese <- read_csv("Assignment/ObesityDataSet_raw_and_data_sinthetic (2)/ObesityDataSet_raw_and_data_sinthetic.csv")
+
+View(obese)
+str(obese)
+
